66b473236ac1358ac2aa2b7c2364099107df1fba
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-08-09 17:56:18 +0200

    wykrywanie konkretnych punktów i tracking po wykresach x i y

---
+++ PoseModule.py
@@ -0,0 +1,61 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class PoseDetector():
+    def __init__(self, mode=False, upBody=False, smooth=True, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+
+    def find_pose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        #print(results.pose_landmarks)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        return img
+
+    def get_position(self, img, draw=True):
+        lm_list = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                height, width, c = img.shape
+                #print(id, lm)
+                cx, cy = int(lm.x * width), int(lm.y * height)
+                lm_list.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255,0,0), cv2.FILLED)
+        return lm_list
+
+
+def main():
+    cap = cv2.VideoCapture("videos/test.mp4")
+    pTime = 0
+    detector = PoseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.find_pose(img)
+        lm_list = detector.find_pose(img)
+        print(lm_list)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0),
+                    3)  # trzy wartosci w nawiasach to kolory rgb
+        cv2.imshow("Image", img)
+
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
-cap = cv2.VideoCapture("test.mp4")
+cap = cv2.VideoCapture("videos/test.mp4")
 pTime = 0
 
 while True:
