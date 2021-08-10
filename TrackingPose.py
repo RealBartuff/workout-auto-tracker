dc56c5e6cf9189486edb227f689071655556511f
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-08-10 20:22:41 +0200

    counter do squat liczony po różnicy, dodano plik requirements

--- TrackingPose.py
+++ TrackingPose.py
@@ -13,26 +13,30 @@ while True:
     success, img = cap.read()
     img = cv2.resize(img, (540, 960))
     # img = cv2.imread("videos/squatt.jpg")
-    img = detector.find_pose(img, draw=False)
+    img = detector.find_pose(img, draw=True)
     lm_list = detector.get_position(img, False)
     if len(lm_list) != 0:
         # lewa noga
         angle = detector.find_angle(img, 23, 25, 27)    #trzy punkty do określenia kąta ze wzoru mediapipe
+        squat_formula = (lm_list[25][2] - lm_list[23][2]) / (lm_list[27][2] - lm_list[25][2])
+        # print("biodro", (lm_list[25][2] - lm_list[23][2]) / (lm_list[27][2] - lm_list[25][2]))
+        # print("kolano", lm_list[25][2])
+        # print("stopa", lm_list[27][2])
         # # prawa noga
         # detector.find_angle(img, 24, 26, 28)
         percent = np.interp(angle, (170, 110), (0, 100))    # zakres ruchu w procentach
         # print(angle, percent)
 
         # if wykonanie przysiadu:
-        if percent == 100:
+        if squat_formula > 0.9:
             if direction == 0:
                 # counter += 0.5
                 direction = 1
-        if percent == 0:
+        if squat_formula < 0.4:
             if direction == 1:
                 counter += 1
                 direction = 0
-        print(counter)
+        # print(counter)
 
         # wyświetlanie powtórzeń na obrazie
         cv2.putText(img, f"{counter}", (50, 200), cv2.FONT_HERSHEY_PLAIN, 15, (255, 0, 0), 5)
