6aaa2a5de13c1e09e492c34417ddb8daad0826cd
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-08-19 19:47:41 +0200

    start programu z guzika, stop nie dziala

--- Exercises.py
+++ Exercises.py
@@ -3,42 +3,48 @@ import numpy as np
 import time
 import PoseModule as pm
 
-cap = cv2.VideoCapture("videos/pompki-przod.mp4")
+
+cap = cv2.VideoCapture(0)
 detector = pm.PoseDetector()
-counter = 0
-direction = 0
+
 p_time = 0
 
-while True:
-    success, img = cap.read()
-    img = cv2.resize(img, (540, 960))
-    # img = cv2.imread("videos/squatt.jpg")
-    img = detector.find_pose(img, draw=False)
-    lm_list = detector.get_position(img, False)
-    if len(lm_list) != 0:
-        angle = detector.find_angle(img, 11, 13, 15)  # trzy punkty do określenia kąta ze wzoru mediapipe
-        pushups_formula = (lm_list[13][2] - lm_list[11][2]) / (lm_list[15][2] - lm_list[13][2])
-        percent = np.interp(angle, (170, 110), (0, 100))    # zakres ruchu w procentach
-        # print(angle, percent)
 
-        # if wykonanie przysiadu:
-        if pushups_formula > 0.9:
-            if direction == 0:
-                # counter += 0.5
-                direction = 1
-        if pushups_formula < 0.4:
-            if direction == 1:
-                counter += 1
-                direction = 0
+def run_program():
+    counter = 0
+    direction = 0
+    while True:
+        success, img = cap.read()
+        # img = cv2.resize(img, (540, 960))
+        # img = cv2.imread("videos/squatt.jpg")
+        img = detector.find_pose(img, draw=True)
+        lm_list = detector.get_position(img, False)
+        if len(lm_list) != 0:
+            angle = detector.find_angle(img, 11, 13, 15)  # trzy punkty do określenia kąta ze wzoru mediapipe
+            pushups_formula = (lm_list[13][2] - lm_list[11][2]) / (lm_list[15][2] - lm_list[13][2])
+            percent = np.interp(angle, (170, 110), (0, 100))    # zakres ruchu w procentach
+            # print(angle, percent)
 
-        # wyświetlanie powtórzeń na obrazie
-        cv2.putText(img, f"{counter}", (50, 200), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 5)
+            # if wykonanie przysiadu:
+            if pushups_formula > 0.9:
+                if direction == 0:
+                    # counter += 0.5
+                    direction = 1
+            if pushups_formula < 0.4:
+                if direction == 1:
+                    counter += 1
+                    direction = 0
 
-    # wyświetlanie fps
-    # c_time = time.time()
-    # fps =1/(c_time - p_time)
-    # p_time = c_time
-    # cv2.putText(img, f"{fps}", (50, 200), cv2.FONT_HERSHEY_PLAIN, 15, (255, 0, 0), 5)
+            # wyświetlanie powtórzeń na obrazie
+            cv2.putText(img, f"{counter}", (50, 200), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 5)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+        # wyświetlanie fps
+        # c_time = time.time()
+        # fps =1/(c_time - p_time)
+        # p_time = c_time
+        # cv2.putText(img, f"{fps}", (50, 200), cv2.FONT_HERSHEY_PLAIN, 15, (255, 0, 0), 5)
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
--- MyApp.py
+++ MyApp.py
@@ -1,21 +1,62 @@
 import kivy
+import sys
+import Exercises
+
 kivy.require('2.0.0')
 
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
 
-class MyApp(App):
+class Design(GridLayout):
+    def __init__(self, **kwargs):
+        super(Design, self).__init__(**kwargs)
+        self.cols = 1
 
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text="First Name: "))
+        self.name = TextInput(multiline=False)
+        self.inside.add_widget(self.name)
+
+        self.inside.add_widget(Label(text="Last Name: "))
+        self.last_name = TextInput(multiline=False)
+        self.inside.add_widget(self.last_name)
+
+        self.inside.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline=False)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+
+        self.start_btn = Button(text="START TRAINING",
+                                font_size=20
+                                )
+        self.start_btn.bind(on_press=self.start_workout)
+        self.add_widget(self.start_btn)
+        self.stop_btn = Button(text="STOP",
+                               font_size=20
+                               )
+        self.stop_btn.bind(on_press=self.pause)
+        self.add_widget(self.stop_btn)
+
+    def start_workout(self, instance):
+        print("Running...")
+        Exercises.run_program()
+
+    def pause(self, instance):
+        print("Stop")
+        sys.exit()
+
+
+class MyApp(App):
     def build(self):
-        parent = Widget()
-        startbtn = Button(text="START \nWORKOUT", size=(100, 100), pos_hint={"x":1, "y":1, "left":1, "right":1,
-                                                                             "center_x":5, "center_y":5, "top":1,
-                                                                             "bottom":1})
-        parent.add_widget(startbtn)
-        return parent
+        return Design()
 
 
 if __name__ == '__main__':
