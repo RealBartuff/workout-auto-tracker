086e91073e8e5b3c907caeac0a57db6d026deea7
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-08-20 22:34:38 +0200

    program w formie kv

--- MyApp.py
+++ MyApp.py
@@ -8,13 +8,18 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.image import Image
 
 
 class WindowManager(ScreenManager):
     pass
 
 
-class Menu(Screen, Widget):
+class Menu(Screen):
+    pass
+
+
+class WorkingScreen(Screen, Widget):
     def start_btn(self):
         print("Running...")
         Exercises.run_program()
@@ -22,10 +27,6 @@ class Menu(Screen, Widget):
     def stop_btn(self):
         print("Stop")
         return
-
-
-class WorkingScreen(Screen):
-    pass
 
 
 class Calendar(Screen):
