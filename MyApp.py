cab9033cab04158e3226cc33390ccd6285a7f937
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-08-21 11:05:23 +0200

    layout kamery osadzony w .kv

--- MyApp.py
+++ MyApp.py
@@ -1,14 +1,19 @@
 import kivy
-import sys
-import Exercises
 
 kivy.require('2.0.0')
 
+import kivy.core.text
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.base import EventLoop
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+import cv2
 
 
 class WindowManager(ScreenManager):
@@ -19,14 +24,34 @@ class Menu(Screen):
     pass
 
 
-class WorkingScreen(Screen, Widget):
-    def start_btn(self):
-        print("Running...")
-        Exercises.run_program()
+class KivyCamera(Image):
 
-    def stop_btn(self):
-        print("Stop")
-        return
+    def __init__(self, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = None
+
+    def start(self, capture, fps=30):
+        self.capture = capture
+        Clock.schedule_interval(self.update, 1.0 / fps)
+
+    def stop(self):
+        Clock.unschedule_interval(self.update)
+        self.capture = None
+
+    def update(self, dt):
+        return_value, frame = self.capture.read()
+        if return_value:
+            texture = self.texture
+            w, h = frame.shape[1], frame.shape[0]
+            if not texture or texture.width != w or texture.height != h:
+                self.texture = texture = Texture.create(size=(w, h))
+                texture.flip_vertical()
+            texture.blit_buffer(frame.tobytes(), colorfmt='bgr')
+            self.canvas.ask_update()
+
+
+class WorkingScreen(Screen):
+    pass
 
 
 class Calendar(Screen):
