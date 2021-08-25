5dced2757072d3b815a93a1b041d7450120dbb7e
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-08-25 19:25:50 +0200

    Ostatnie konsultacje z Robertem - def update umieszczony w klasach

--- MyApp.py
+++ MyApp.py
@@ -7,6 +7,7 @@ kivy.require('2.0.0')
 import kivy.core.text
 import numpy as np
 import time
+from datetime import date
 import PoseModule as pm
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -27,8 +28,6 @@ import sqlite3
 detector = pm.PoseDetector()
 conn = sqlite3.connect("record_trackDB.db")
 c = conn.cursor()
-p_cnt = 0
-s_cnt = 0
 
 
 class WindowManager(ScreenManager):
@@ -40,25 +39,81 @@ class Menu(Screen):
         sys.exit(MyMainApp)
 
 
+class Exercises:
+    def __init__(self, **kwargs):
+        super(Exercises, self).__init__(**kwargs)
+        self.counter = 0
+        self.direction = 0
+
+    def is_motion(self, lm_list):
+        return lm_list[0] and lm_list[27]
+
+
+class PushUps(Exercises):
+    def do_rep(self, lm_list):
+        pushup_formula = (lm_list[13][2] - lm_list[11][2]) / (lm_list[15][2] - lm_list[13][2])
+        if pushup_formula > 0.9:
+            self.direction = 1
+        else:
+            self.direction = 0
+
+        if pushup_formula < 0.35:
+            self.counter += 1
+            self.direction = 0
+        else:
+            self.direction = 1
+
+    def show_reps(self, img):
+        # label =
+        cv2.putText(img, f"{self.counter}", (20, 200), cv2.FONT_HERSHEY_DUPLEX, 4, (0, 205, 150), 5)
+
+
+class SitUps(Exercises):
+    def do_rep(self, lm_list):
+        squat_formula = (lm_list[25][2] - lm_list[23][2]) / (lm_list[27][2] - lm_list[25][2])
+        if squat_formula > 0.9:
+            self.direction = 1
+        else:
+            self.direction = 0
+
+        if squat_formula < 0.35:
+            self.counter += 1
+            self.direction = 0
+        else:
+            self.direction = 1
+
+    def show_reps(self, img):
+        cv2.putText(img, f"{self.counter}", (20, 100), cv2.FONT_HERSHEY_DUPLEX, 4, (205, 150, 0), 5)
+
+
 class KivyCamera(Image):
 
     def __init__(self, **kwargs):
         super(KivyCamera, self).__init__(**kwargs)
         self.cap = None
-        self.push_counter = 0
-        self.sit_counter = 0
-        self.p_direction = 0
-        self.s_direction = 0
         self.event = None
+        self.push_ups = PushUps()
+        self.sit_ups = SitUps()
 
     def start(self, cap, fps=30):
         self.cap = cap
+        self.start_day()
         self.event = Clock.schedule_interval(self.update, 1.0 / fps)
 
     def stop(self):
         if self.event:
             Clock.unschedule(self.event)
         self.cap = None
+
+    def start_day(self):
+        c.execute('CREATE TABLE IF NOT EXISTS RecordONE (Pushups INTEGER, Situps INTEGER, Date TEXT)')
+        today = date.today()
+        c.execute("select * from RecordOne where Date=?", (today, ))
+        if not len(c.fetchall()):
+            c.execute("INSERT INTO RecordONE (Date) VALUES(?)", (today,))
+        conn.commit()
+        c.close()
+        conn.close()
 
     def update(self, dt):
         return_value, frame = self.cap.read()
@@ -69,32 +124,9 @@ class KivyCamera(Image):
             angle2 = detector.find_angle(img, 23, 25, 27)# trzy punkty do określenia kąta ze wzoru mediapipe
             percent_pu = np.interp(angle1, (190, 270), (0, 100))  # zakres ruchu w procentach
             percent_su = np.interp(angle2, (170, 110), (0, 100))
-            try:
-                pushup_formula = (lm_list[13][2] - lm_list[11][2]) / (lm_list[15][2] - lm_list[13][2])
-                if pushup_formula > 0.9:
-                    if self.p_direction == 0:
-                        # counter += 0.5
-                        self.p_direction = 1
-                if pushup_formula < 0.35:
-                    if self.p_direction == 1:
-                        self.push_counter += 1
-                        self.p_direction = 0
-
-                squat_formula = (lm_list[25][2] - lm_list[23][2]) / (lm_list[27][2] - lm_list[25][2])
-                if squat_formula > 0.9:
-                    if self.s_direction == 0:
-                        # counter += 0.5
-                        self.s_direction = 1
-                if squat_formula < 0.35:
-                    if self.s_direction == 1:
-                        self.sit_counter += 1
-                        self.s_direction = 0
-            except ZeroDivisionError:
-                pass
-
-            # wyświetlanie powtórzeń na obrazie
-            cv2.putText(img, f"{self.sit_counter}", (20, 100), cv2.FONT_HERSHEY_DUPLEX, 4, (205, 50, 0), 5)
-            cv2.putText(img, f"{self.push_counter}", (20, 200), cv2.FONT_HERSHEY_DUPLEX, 4, (0, 205, 50), 5)
+            for exercise in [self.push_ups, self.sit_ups]:
+                if exercise.is_motion(lm_list):
+                    exercise.do_rep(lm_list)
 
         if return_value:
             texture = self.texture
@@ -124,13 +156,19 @@ class WorkingScreen(Screen, BoxLayout):
 
 
 class P(FloatLayout):
-    pass
+    def set_goals(self):
+        pompki = self.ids.ile_pompek.text
+        przysiady = self.ids.ile_przysiad.text
+        print("Pompki: ", pompki, "Przysiady: ", przysiady)
+        self.ids.ile_pompek.text = ""
+        self.ids.ile_przysiad.text = ""
 
 
 class Calendar(Screen, Widget):
     def push_counter(self):
         c.execute('SELECT Pushups FROM RecordONE')
         data = c.fetchall()[-1]
+        # self.ids.circle_bar.max =
         current = self.ids.circle_bar.value
         current += data
         self.ids.circle_bar.value = current
@@ -140,6 +178,7 @@ class Calendar(Screen, Widget):
     def sit_counter(self):
         c.execute('SELECT Situps FROM RecordONE')
         data = c.fetchall()[-1]
+        # self.ids.line_bar.max =
         current = self.ids.line_bar.value
         current += data
         self.ids.line_bar.value = current
@@ -155,24 +194,6 @@ class Calendar(Screen, Widget):
 kv = Builder.load_file("my.kv")
 
 
-def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS RecordONE (Pushups INTEGER, Situps INTEGER)')
-
-
-def data_entry():
-    pushups = KivyCamera().push_counter
-    situps = KivyCamera().sit_counter
-    c.execute("INSERT INTO RecordONE (Pushups, Situps) VALUES(?, ?)", (pushups, situps))
-    conn.commit()
-
-
-create_table()
-data_entry()
-
-c.close()
-conn.close()
-
-
 class MyMainApp(App):
     def build(self):
         return kv
@@ -180,3 +201,9 @@ class MyMainApp(App):
 
 if __name__ == '__main__':
     MyMainApp().run()
+
+
+
+"""update RecordOne set pushups = pushups+1 where date="2021-08-25"
+select * from RecordOne where Date="2021-08-25"
+select * from RecordOne where Date="2021-08-25" limit 1"""
